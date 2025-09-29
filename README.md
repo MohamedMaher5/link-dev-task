c7644181120e69f5e2a3e24b237fb2d12272f151
Mohamed Maher <mohamedmaher055@gmail.com>
2025-09-30 02:05:21 +0300

    Export excel sheet

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 Start the application using Docker Compose:
 
 ```bash
-docker compose -f docker-compose.dev.yml up -d
+docker compose -f docker-compose.dev.yml up -d --build
 ```
 
 ### Access Points
@@ -27,7 +27,7 @@ A Postman collection is available for testing the API endpoints:
 
 1. Import the collection file: `Link-Dev-Task.postman_collection.json` (located in `/docs` folder)
 2. Import the environment file: `Link-Dev-Task-Env.postman_environment.json` (located in `/docs` folder)
-3. Set the base URL environment variable to: `http://127.0.0.1:4000`
+3. Set the base URL environment variable to: `http://127.0.0.1:4000/api/`
 4. Start making requests to test the API
 
 Alternatively, you can use the API documentation at http://127.0.0.1:4000/docs/api# to explore and test endpoints directly in your browser.
--- code/app/Exports/SingleReportSheet.php
+++ code/app/Exports/SingleReportSheet.php
@@ -4,21 +4,31 @@ namespace App\Exports;
 
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
 use Maatwebsite\Excel\Concerns\WithTitle;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class SingleReportSheet implements FromCollection, WithTitle, WithHeadings
+class SingleReportSheet implements FromCollection, WithTitle, WithHeadings, WithStyles, ShouldAutoSize
 {
     protected string $title;
     protected Collection $data;
 
-    public function __construct(string $title, $data)
+    public function __construct(string $title, Collection $data)
     {
         $this->title = $title;
-        $this->data = collect($data);
+
+        $this->data = collect($data)->map(function ($row) {
+            if (is_object($row) && method_exists($row, 'toArray')) {
+                return $row->toArray();
+            }
+            return (array) $row;
+        });
     }
 
-    public function collection()
+    public function collection(): Collection
     {
         return $this->data;
     }
@@ -33,5 +43,21 @@ class SingleReportSheet implements FromCollection, WithTitle, WithHeadings
         return $this->data->first()
             ? array_keys((array) $this->data->first())
             : [];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            1 => [
+                'font' => [
+                    'bold' => true,
+                    'color' => ['rgb' => 'FFFFFF'],
+                ],
+                'fill' => [
+                    'fillType' => Fill::FILL_SOLID,
+                    'startColor' => ['rgb' => '4F81BD'],
+                ],
+            ],
+        ];
     }
 }
